ef2cd76c8e26c2ae8ef8972b60cd4ec96f583419
ReachEmpireBot <141654681+henglaw1991@users.noreply.github.com>
2026-08-09 23:48:33 +0700

    Add files via upload

---
+++ fortradex_vercel_ready/partials/header.php
@@ -0,0 +1,186 @@
+<header class="main-header header-style-one">
+    <!-- header-top -->
+    <div class="header-top">
+        <div class="large-container">
+            <div class="top-inner">
+                <div class="support-box">
+                    <div class="icon-box"><i class="icon-07"></i></div>
+                    <a href="tel:912345678">91 2345 678</a>
+                </div>
+                <div class="option-block">
+                    <div class="language-picker js-language-picker mr_40" data-trigger-class="btn btn--subtle">
+                        <form action="index-2.php" class="language-picker__form">
+                            <label for="language-picker-select">Select your language</label>
+                            <select name="language-picker-select" id="language-picker-select">
+                                <option lang="de" value="deutsch"></option>
+                                <option lang="en" value="english" selected></option>
+                                <option lang="fr" value="francais"></option>
+                                <option lang="it" value="italiano"></option>
+                            </select>
+                        </form>
+                    </div>
+                    <a href="index.php" class="theme-btn btn-one mr_10">Open Account</a>
+                    <a href="index.php" class="theme-btn btn-two">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- header-lower -->
+    <div class="header-lower">
+        <div class="large-container">
+            <div class="outer-box">
+                <figure class="logo-box"><a href="index.php"><img src="assets/images/logo.png" alt=""></a></figure>
+                <div class="menu-area">
+                    <!--Mobile Navigation Toggler-->
+                    <div class="mobile-nav-toggler">
+                        <i class="icon-bar"></i>
+                        <i class="icon-bar"></i>
+                        <i class="icon-bar"></i>
+                    </div>
+                    <nav class="main-menu navbar-expand-md navbar-light clearfix">
+                        <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                            <ul class="navigation clearfix">
+                                <li class="current dropdown"><a href="index.php">Home</a>
+                                    <ul>
+                                        <li><a href="index.php">Home One</a></li>
+                                        <li><a href="index-2.php">Home Two</a></li>
+                                        <li><a href="index-3.php">Home Three</a></li>
+                                        <li><a href="index-4.php">Home Four</a></li>
+                                        <li><a href="index-5.php">Home Five</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="index.php">Trading</a>
+                                    <ul>
+                                        <li><a href="platform.php">Platform</a></li>
+                                        <li><a href="account.php">Account</a></li>
+                                        <li><a href="account-details.php">Account Deatils</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="index.php">Market</a>
+                                    <ul>
+                                        <li><a href="markets.php">Markets Place</a></li>
+                                        <li><a href="markets-details.php">Markets Details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="index.php">About Us</a>
+                                    <ul>
+                                        <li class="dropdown"><a href="index.php">Education</a>
+                                            <ul>
+                                                <li><a href="education.php">Education</a></li>
+                                                <li><a href="education-details.php">Book Details</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown"><a href="index.php">Team</a>
+                                            <ul>
+                                                <li><a href="team.php">Our Expert Team</a></li>
+                                                <li><a href="team-details.php">Team Deatils</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="about.php">About Us</a></li>
+                                        <li><a href="faq.php">FAQ's</a></li>
+                                        <li><a href="error.php">404</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="index.php">Blog</a>
+                                    <ul>
+                                        <li><a href="blog.php">Blog Grid</a></li>
+                                        <li><a href="blog-2.php">Blog Standard</a></li>
+                                        <li><a href="blog-details.php">Blog Details</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact.php">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <div class="search-btn ml_30"><button class="search-toggler"><i class="icon-10"></i></button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- header-bottom -->
+    <div class="header-bottom">
+        <div class="large-container">
+            <div class="bottom-inner">
+                <div class="inner-box">
+                    <ul class="stock-list">
+                        <li class="upper">US30 394833.90 <span>(+0.93 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391323.90 <span>(-0.12 <i class="icon-05"></i>)</span></li>
+                        <li class="lower">US30 391467.90 <span>(-0.35 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 354033.90 <span>(+0.55 <i class="icon-06"></i>)</span></li>
+                        <li class="upper">US30 394346.90 <span>(+0.76 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391300.90 <span>(-0.43 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 394833.90 <span>(+0.93 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391323.90 <span>(-0.12 <i class="icon-05"></i>)</span></li>
+                        <li class="lower">US30 391467.90 <span>(-0.35 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 354033.90 <span>(+0.55 <i class="icon-06"></i>)</span></li>
+                        <li class="upper">US30 394346.90 <span>(+0.76 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391300.90 <span>(-0.43 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 394833.90 <span>(+0.93 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391323.90 <span>(-0.12 <i class="icon-05"></i>)</span></li>
+                        <li class="lower">US30 391467.90 <span>(-0.35 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 354033.90 <span>(+0.55 <i class="icon-06"></i>)</span></li>
+                        <li class="upper">US30 394346.90 <span>(+0.76 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391300.90 <span>(-0.43 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 394833.90 <span>(+0.93 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391323.90 <span>(-0.12 <i class="icon-05"></i>)</span></li>
+                        <li class="lower">US30 391467.90 <span>(-0.35 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 354033.90 <span>(+0.55 <i class="icon-06"></i>)</span></li>
+                        <li class="upper">US30 394346.90 <span>(+0.76 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391300.90 <span>(-0.43 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 394833.90 <span>(+0.93 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391323.90 <span>(-0.12 <i class="icon-05"></i>)</span></li>
+                        <li class="lower">US30 391467.90 <span>(-0.35 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 354033.90 <span>(+0.55 <i class="icon-06"></i>)</span></li>
+                        <li class="upper">US30 394346.90 <span>(+0.76 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391300.90 <span>(-0.43 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 394833.90 <span>(+0.93 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391323.90 <span>(-0.12 <i class="icon-05"></i>)</span></li>
+                        <li class="lower">US30 391467.90 <span>(-0.35 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 354033.90 <span>(+0.55 <i class="icon-06"></i>)</span></li>
+                        <li class="upper">US30 394346.90 <span>(+0.76 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391300.90 <span>(-0.43 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 394833.90 <span>(+0.93 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391323.90 <span>(-0.12 <i class="icon-05"></i>)</span></li>
+                        <li class="lower">US30 391467.90 <span>(-0.35 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 354033.90 <span>(+0.55 <i class="icon-06"></i>)</span></li>
+                        <li class="upper">US30 394346.90 <span>(+0.76 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391300.90 <span>(-0.43 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 394833.90 <span>(+0.93 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391323.90 <span>(-0.12 <i class="icon-05"></i>)</span></li>
+                        <li class="lower">US30 391467.90 <span>(-0.35 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 354033.90 <span>(+0.55 <i class="icon-06"></i>)</span></li>
+                        <li class="upper">US30 394346.90 <span>(+0.76 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391300.90 <span>(-0.43 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 394833.90 <span>(+0.93 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391323.90 <span>(-0.12 <i class="icon-05"></i>)</span></li>
+                        <li class="lower">US30 391467.90 <span>(-0.35 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 354033.90 <span>(+0.55 <i class="icon-06"></i>)</span></li>
+                        <li class="upper">US30 394346.90 <span>(+0.76 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391300.90 <span>(-0.43 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 394833.90 <span>(+0.93 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391323.90 <span>(-0.12 <i class="icon-05"></i>)</span></li>
+                        <li class="lower">US30 391467.90 <span>(-0.35 <i class="icon-05"></i>)</span></li>
+                        <li class="upper">US30 354033.90 <span>(+0.55 <i class="icon-06"></i>)</span></li>
+                        <li class="upper">US30 394346.90 <span>(+0.76 <i class="icon-06"></i>)</span></li>
+                        <li class="lower">US30 391300.90 <span>(-0.43 <i class="icon-05"></i>)</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--sticky Header-->
+    <div class="sticky-header">
+        <div class="large-container">
+            <div class="outer-box">
+                <figure class="logo-box"><a href="index.php"><img src="assets/images/logo.png" alt=""></a></figure>
+                <div class="menu-area">
+                    <nav class="main-menu clearfix">
+                        <!--Keep This Empty / Menu will come through Javascript-->
+                    </nav>
+                    <div class="search-btn ml_30"><button class="search-toggler"><i class="icon-10"></i></button></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
